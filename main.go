b2ad2f10c3a181f6746693e7962f8daf9a02418a
Per Jahn <perjahn@gmail.com>
2025-05-10 07:51:07 +0200

    2nd

--- main.go
+++ main.go
@@ -51,6 +51,26 @@ type ArtifactoryPermissionDetailsTargets struct {
 	ExcludePatterns []string `json:"exclude_patterns"`
 }
 
+type ArtifactoryUsers struct {
+	Users []ArtifactoryUser `json:"users"`
+}
+
+type ArtifactoryUser struct {
+	Username string `json:"username"`
+	Uri      string `json:"uri"`
+	Realm    string `json:"realm"`
+	Status   string `json:"status"`
+}
+
+type ArtifactoryGroups struct {
+	Groups []ArtifactoryGroup `json:"groups"`
+}
+
+type ArtifactoryGroup struct {
+	GroupName string `json:"group_name"`
+	Uri       string `json:"uri"`
+}
+
 type Repo struct {
 	Key                 string `json:"key"`
 	Description         string `json:"description"`
@@ -101,6 +121,32 @@ func main() {
 	//		permission.Name, permission.Uri)
 	//}
 
+	users, err := getUsers(baseurl, token)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("User count:", len(users))
+
+	//for _, user := range users {
+	//	fmt.Printf("Username: %s, Uri: %s, Realm: %s, Status: %s\n",
+	//		user.Username, user.Uri, user.Realm, user.Status)
+	//}
+
+	groups, err := getGroups(baseurl, token)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Group count:", len(groups))
+
+	//for _, group := range groups {
+	//	fmt.Printf("Name: %s, Uri: %s\n",
+	//		group.GroupName, group.Uri)
+	//}
+
 	//permissions := []ArtifactoryPermission{
 	//	{Name: "permission1", Uri: "/access/api/v2/permissions/permission1"},
 	//	{Name: "permission2", Uri: "/access/api/v2/permissions/permission2"},
@@ -126,6 +172,84 @@ func main() {
 		fmt.Println("Error saving permission details:", err)
 		os.Exit(1)
 	}
+}
+
+func getUsers(baseurl, token string) ([]ArtifactoryUser, error) {
+	url := baseurl + "/access/api/v2/users"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %w", err)
+	}
+
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error sending request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response body: %w", err)
+	}
+
+	fmt.Printf("Response status: %s\n", resp.Status)
+	//fmt.Printf("Response body: %s\n", body)
+
+	err = os.WriteFile("allusers.json", []byte(body), 0600)
+	if err != nil {
+		return nil, fmt.Errorf("error saving response body: %w", err)
+	}
+
+	var users ArtifactoryUsers
+
+	err = json.Unmarshal(body, &users)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing response body: %w", err)
+	}
+
+	return users.Users, nil
+}
+
+func getGroups(baseurl, token string) ([]ArtifactoryGroup, error) {
+	url := baseurl + "/access/api/v2/groups"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %w", err)
+	}
+
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error sending request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response body: %w", err)
+	}
+
+	fmt.Printf("Response status: %s\n", resp.Status)
+	//fmt.Printf("Response body: %s\n", body)
+
+	err = os.WriteFile("allgroups.json", []byte(body), 0600)
+	if err != nil {
+		return nil, fmt.Errorf("error saving response body: %w", err)
+	}
+
+	var groups ArtifactoryGroups
+
+	err = json.Unmarshal(body, &groups)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing response body: %w", err)
+	}
+
+	return groups.Groups, nil
 }
 
 func getRepos(baseurl string, token string) ([]ArtifactoryRepo, error) {
